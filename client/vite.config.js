e1234c612bd3272d710b6b44491063e4f2dc8dc9
Usama Bin Amjad <mohammadosama6666@gmail.com>
2025-09-29 16:18:46 +0500

    frontend/routes and structure

---
+++ client/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
+});
