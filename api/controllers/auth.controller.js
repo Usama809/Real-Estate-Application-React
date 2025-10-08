65ff7454856070b6bdbc5235f9edb543edf11f73
Usama Bin Amjad <mohammadosama6666@gmail.com>
2025-10-08 10:45:36 +0500

    Add redux toolkit for SignInPage

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,10 +2,11 @@ import User from "../models/user-model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
+
 //  SingUp
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null, // Changed from currentState for clarity
+  error: null,
+  loading: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+
+  reducers: {
+    SignInStart: (state) => {
+      state.loading = true;
+    },
+    SignInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    SignInFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
+
+export const { SignInStart, SignInSuccess, SignInFailure } = userSlice.actions;
+export default userSlice.reducer;
